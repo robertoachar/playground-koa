30741a9691cca0cb14b23970dd71c6f33e266999
Roberto Achar <robertoachar@gmail.com>
2019-04-20 15:52:43 -0300

    feature: add koa

--- src/index.js
+++ src/index.js
@@ -1,3 +1,7 @@
+const app = require('./app');
+const config = require('./config');
 const logger = require('./logger');
 
-logger.info('Hello Logger');
+app.listen(config.PORT, () => {
+  Object.keys(config).map((key) => logger.info(`${key}: ${config[key]}`));
+});
